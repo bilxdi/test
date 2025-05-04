b3f0f4d869262b532ff86274ddef9b9bafc4317a
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2025-05-04 11:03:00 +0700

    nambah index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $name="biladi";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman awal</title>
+</head>
+<body>
+    <h1>halooo<?php echo $name ?></h1>
+</body>
+</html>
